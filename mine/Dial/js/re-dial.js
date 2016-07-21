f29681610a7ccdeb304b793875bdf74f450eddab
Songyc <419973879@qq.com>
2016-07-21 18:48:15 +0800

    Dial

--- mine/Dial/js/re-dial.js
+++ mine/Dial/js/re-dial.js
@@ -2,7 +2,6 @@
 	'use strict';
 
 	var doc = document,
-		html = doc.documentElement,
 		hasTouch = 'ontouchstart' in window,
 		touchstart = hasTouch ? 'touchstart' : 'mousedown',
 	    touchmove = hasTouch ? 'touchmove' : 'mousemove',
@@ -60,11 +59,13 @@
 		eachAngle: 0,
 		radius: 0,
 		click: true,
-		position: null
+		position: null,
+		alwaysUp: true
 	}
 
 	Dial.prototype = {
 		constructor: Dial,
+		uuid: 0,
 		handleEvent: function (e) {
 			switch(e.type) {
 				case touchstart: 
@@ -88,15 +89,13 @@
 			
 			if(el.nodeType === 1) {
 
+				c.initAngle %= 360;
 				this.target = el; 							
-
-				this.dialInitAngle = c.initAngle - 90;
-
-				this.angle = this.initAngle;
-
-				this._rotate(c.initAngle);	
-
+				this.dialInitAngle = (c.initAngle - 90) % 360; 			
+				this.angle = c.initAngle || 0;
+				this._rotate(c.initAngle);
 				this._position(el);
+				this.center = this._center(el);
 
 				if(cb) {
 					var width, height, 
@@ -131,6 +130,8 @@
 
 						ele.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 						ele.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
+
+						this._data(el, {transform: y, webkitTransform: y});
 						
 					}
 
@@ -152,9 +153,9 @@
 				bottom: '100%',
 				center: '50%'
 			};
-
+			// 将位置字符串转成数组
 			p = p.trim().split(' ');
-
+			// 如果数组第一个位置为top或者bottom, 则要将两个位置调换。比如['top', 'left']，换成['left', 'top']。
 			if(/^(t|b)/.test(p[0])) {
 				t = p[0];
 				p[0] = p[1];
@@ -180,114 +181,202 @@
 			}
 		},
 
-		_rotate: function (angle, el) {
-			if(angle == null) {
-				return;
+		_data: function (el, name, data) {
+			var cache = this.cache,
+				id, ec, key;
+
+			if(!cache) {
+				this.cache = cache = {};
 			}
 
-			el = el || this.target;
+			id = el.uuid;
 
-			el.style.transform = 'rotate(' + angle + 'deg)';
-			el.style.webkitTransform = 'rotate(' + angle + 'deg)';
-			el.style.transition = 'transform .3s ease-in';
-			el.style.webkitTransition = '-webkit-transform .3s ease-in';
+			if(!id) {
+				el.uuid = id = this.uuid++;
+			}
+
+			ec = cache[id] = {};
+
+			if(name == null) {
+				delete cache[id];
+			}
+
+			if(data != null) {
+				ec[name] = data;
+				return ec[name];
+
+			}else if( _toString.call(name) === '[object Object]' ){
+				extend(ec, name);
+			}
 		},
 
+		_matrix: (function () {
+	        var s, scs,
+	            cf = {
+	                translate: function (t, el) {
+	                    if(typeof t === 'number') {
+	                        t = { x: t, y: t }
+	                    }
+
+	                    return ("translate(" + t.x + "px, " + t.y + "px)");
+	                },
+	                rotate: function (r) {
+	                    return ("rotate(" + r + "deg)");
+	                },
+	                scale: function (sc) {
+	                    if(typeof sc === 'number') {
+	                        scs = "scale(" + sc + ")";
+	                    }else {
+	                        scs += "scale(" + sc.x + ", " + sc.y + ")";
+	                    }
+
+	                    return scs;
+	                },
+	                skew: function (sk) {
+	                    return ("skew(" + sk + "deg)");
+	                }
+	            };
+
+	        return function (el, prop) {
+	        	var rst = [];
+
+	            for(var key in prop) {
+
+	            	if(prop[key] === true) {
+	            		prop[key] = this._data(el, key);
+	            	}
+	            	
+	                s = cf[key].call(this, prop[key], el);
+	                rst.push(s);
+	            }
+
+	            this._data(el, prop);
+
+	            s = rst.join(" ");
+	            
+	            el.style.transform = s;
+	            el.style.webkitTransform = s;
+	            el.style.transition = 'transform .3s ease-in';
+	            el.style.webkitTransition = '-webkit-transform .3s ease-in';
+	        }
+	    })(),
+
 		_start: function (e) {
-			var point = hasTouch ? e.touches[0] : e,
-				x, y, center;				
+			var point = hasTouch ? e.touches[0] : e;
 
  			e.preventDefault();
 
-			x = point.clientX;
-			y = point.clientY;
-			center = this._center(this.target);
+ 			// 记录开始点击的坐标
+			this.startX = point.clientX;
+			this.startY = point.clientY;
 
-			this.startX = x;
-			this.startY = y;
-			this.center = center;
-
+			// 绑定事件
 			this._bind(touchmove);
 			this._bind(touchend);
 		},
 
 		_move: function (e) {
 			var point = hasTouch ? e.touches[0] : e,
-				sAngle, cAngle, 
-				lAngel = this.lAngel || 0, 
-				angle = this.angle || 0,
-				slideAngle;  
+				sag, cag, lag, 
+				alwaysUp = this.config.alwaysUp,
+				ag = this.angle;
 
 			e.preventDefault();
 
-			sAngle = this._angle({x: this.startX, y: this.startY}, this.center);
-			cAngle = this._angle({x: point.clientX, y: point.clientY}, this.center);
+			// 开始点与转盘中心连线与x轴正方向的夹角
+			sag = this._angle({x: this.startX, y: this.startY}, this.center);
 
-			if(cAngle > 0 && (cAngle - lAngel >= 180)) {			// 从第三象限滑动到第二象限
-				cAngle -= 360;
+			// 滑动点与转盘中心连线与x轴正方向的夹角
+			cag = this._angle({x: point.clientX, y: point.clientY}, this.center);
+
+			lag = this.lAngel || sag;
+
+			// 从第三象限滑动到第二象限。即从180度转到-180度，会出现逆时针旋转一周的bug。修正变量cag
+			if(cag > 0 && (cag - lag >= 180)) {				
+				cag -= 360;
 			}
 
-			if(cAngle < 0 && (cAngle - lAngel <= -180)) { 			// 从第二象限滑动到第三象限
-				cAngle += 360;
+			// 从第二象限滑动到第三象限。即从-180度转到180度，会出现顺时针旋转一周的bug。修正变量cag
+			if(cag < 0 && (cag - lag <= -180)) {			
+				cag += 360;
 			}
 
-			angle += (slideAngle = cAngle - sAngle);
+			// 计算滑动的角度，滑动角度 = 当前角度 - 开始角度。之前的滑动角度再加上本次的滑动角度
+			ag += cag - lag;
 
-			this._rotate(angle);
+			// 旋转
+			this._rotate(ag);
 
-			this.slideAngle = slideAngle;
-			this.lAngel = cAngle;
+			if(alwaysUp) {
+				var b = this.config.block;
+				for(var i = 0, l = b.length; i < l; i++) {
+					// this._rotate(b[i], 0, -ag);
+				}
+			}
+
+			// 记录当前角度为上一次角度
+			this.lAngel = cag;
+
+			// 记录总滑动角度
+			this.angle = ag;
+
+			// 标记滑动状态
 			this.move = true;
 		},
 
 		_end: function (e) {
 			var point = hasTouch ? ( e.touches[0] ? e.touches[0] : e.changedTouches[0] ) : e,
 				c = this.config,
-				angle = this.angle,
-				ct = c.block, l, eAngle,
-				slideAngle = this.slideAngle || 0,
-				eachAngle = c.eachAngle,
-				dia = this.dialInitAngle % 360;
+				ct = c.block, l, eag,
+				ecag = c.eachAngle, slag = 0,
+
+				// 转盘的初始角度相对于x轴的角度, 也是转盘的中心轴。用户设置的初始角度可能过大或过小，要除以360度取余。
+				dia = this.dialInitAngle;
 
 			e.preventDefault();
 
 			this.endX = point.clientX;
 			this.endY = point.clientY;
 
-			if(c.click && ct && (l = ct.length) && !this.move) { 				// 如果支持点击块元素旋转，并且用户没有滑动(模拟点击事件)
+			// 如果支持点击块元素旋转，并且用户没有滑动(模拟点击事件)。
+			if(c.click && ct && (l = ct.length) && !this.move) { 				
 				for(var i = 0; i < l; i++) {
-					if((ct[i] === e.target || ct[i].contains(e.target))) { 		// 如果点击某个块元素区域						
-						eAngle = this._angle({x: point.clientX, y: point.clientY}, this.center);
 
-						if(dia > 0) {
-							if(eAngle < 0) {
-								eAngle += 360;
-							}
+					// 如果点击某个块元素或者块元素的子孙元素
+					if((ct[i] === e.target || ct[i].contains(e.target))) {
 
-							if(dia < 0) {
-								dia += 360;
-							}							
+						// 获取结束点和转盘中心的连线与x轴正方向的角度
+						eag = this._angle({x: point.clientX, y: point.clientY}, this.center);
+
+						// 如果结束角度小于中心轴角度。要修正结束角度。
+						if(eag < dia) {
+							eag += 360;
 						}
 
-						if((eAngle - dia ) < 180) {
-							slideAngle -= Math.round((eAngle - dia) / eachAngle) * eachAngle;
+						// 结束角度减去中心轴角度小于180，说明结束点离中心轴较近。要逆时针方向旋转。
+						if((eag - dia ) <= 180) {
+							slag -= Math.round((eag - dia) / ecag) * ecag;
 						}
 
-						if((eAngle - dia) > 180) {
-							slideAngle +=  Math.round((360 - (eAngle - dia)) / eachAngle) * eachAngle; 
+						// 结束角度减去中心轴角度大于180，说明结束点离中心轴较远。要顺时针方向旋转。
+						if((eag - dia) > 180) {
+							slag +=  Math.round((360 - (eag - dia)) / ecag) * ecag;
 						}
 
-						angle += slideAngle;
+						// 之前滑动角度加上本次滑动角度。调用this._rotate(angle)方法旋转
 
-						this._rotate(angle);
+						this._rotate( (this.angle += slag) );
+
+						// 跳出本次循环
 						break;
 					}
 				}
 			}
 
-			this.angle = angle;
-			this.slideAngle = 0;
-			this.lAngel = 0;
+			// 清空上一次滑动角度
+			this.lAngel = null;
+
+			// 标记this.move为false，指示转盘不是在滑动状态 						
 			this.move = false;
 		},
 
@@ -299,13 +388,29 @@
 		},
 	}	
 
+	"translate rotate scale skew".split(" ").forEach(function (item, i) {
+		Dial.prototype["_" + item] = function (prop, el) {
+			var obj = {};
+
+			if(!prop) {
+				return;
+			}
+
+			el = el || this.target;
+
+			obj[item] = prop;
+
+			this._matrix.call(this, el, obj);
+		}
+	});
+
 	// test 
 	new Dial({
 		target: "#Jtab_ctrl",
 		initAngle: 0,
 		block: '#Jtab_ctrl .block',
-		radius: 300,
-		click: false,
+		radius: 280,
 		position: 'center center'
-	})
+	});
+
 })(this);
